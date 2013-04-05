2adc1cea6cbb049a712b00347bb3d9184705c067
klarrimore <klarrimore@icehook.com>
2013-04-05 00:05:23 -0400

    added some site-cookbooks

--- site-cookbooks/centos/metadata.rb
+++ site-cookbooks/centos/metadata.rb
@@ -5,6 +5,8 @@ license          'All rights reserved'
 description      'Installs/Configures centos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe "centos", "Sets up packages"
 recipe "centos::iptables", "Sets up iptables"
 recipe "centos::hosts", "Sets up hosts file"
 
